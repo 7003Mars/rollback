720e0650caed0666dca57fd13b421e62320665bf
Mars <>
2023-06-04 21:40:46 +0800

    Add support for processor logic

--- src/me/mars/rollback/Listeners.kt
+++ src/me/mars/rollback/Listeners.kt
@@ -15,11 +15,13 @@ import mindustry.game.EventType.*
 import mindustry.game.Team
 import mindustry.gen.Building
 import mindustry.gen.Groups
+import mindustry.logic.LAccess
 import mindustry.logic.LExecutor
 import mindustry.logic.TileLayer
 import mindustry.world.Block
 import mindustry.world.blocks.ConstructBlock
 import mindustry.world.blocks.ConstructBlock.ConstructBuild
+import mindustry.world.blocks.logic.LogicBlock.LogicBuild
 import mindustry.world.blocks.storage.CoreBlock.CoreBuild
 
 fun addListeners() {
@@ -127,32 +129,55 @@ fun addListeners() {
         }
     }
 
+    Events.on(TileChangeEvent::class.java) {
+        val build: LogicBuild = it.tile.build.takeIf { b -> b is LogicBuild } as LogicBuild? ?: return@on
+        if (build.executor !is Executor) {
+            // Should happen on the tick the block is set, therefore no need to clone LExecutor fields
+            build.executor = Executor()
+        }
+    }
+
 }
 
 class Executor : LExecutor() {
     override fun runOnce() {
         // TODO: Coded while half awake, please check through
-        val instr: LInstruction = this.instructions[(this.counter.numval+1).toInt()]
-        if (instr is SetBlockI) {
-            if (instr.layer == TileLayer.block) {
-                val b: Block = this.obj(instr.block).takeIf { it is Block } as Block? ?: return
-                val x: Int = this.numi(instr.x)
-                val y: Int = this.numi(instr.y)
-                val sx: Int = x+b.sizeOffset
-                val sy: Int = y+b.sizeOffset
-                val added: ObjectSet<Building> = ObjectSet()
-                for (_x in sx until sx+b.size) {
-                    for (_y in sy until sy+b.size) {
-                        val build: Building = Vars.world.build(_x, _y) ?: continue
-                        if (build is ConstructBuild || added.contains(build)) continue
-                        tileStore.setAction(DeleteAction("", build.pos(), build.block.size, build.team))
-                        added.add(build)
+        var index: Int = (this.counter.numval+1).toInt()
+        if (index >= this.instructions.size || index < 0) index = 0
+        val instr: LInstruction = this.instructions[index]
+        // TOOD: Check for configs too
+        when (instr) {
+            is SetBlockI -> {
+                if (instr.layer == TileLayer.block) {
+                    val b: Block = this.obj(instr.block).takeIf { it is Block } as Block? ?: return
+                    val x: Int = this.numi(instr.x)
+                    val y: Int = this.numi(instr.y)
+                    val sx: Int = x+b.sizeOffset
+                    val sy: Int = y+b.sizeOffset
+                    val added: ObjectSet<Building> = ObjectSet()
+                    for (_x in sx until sx+b.size) {
+                        for (_y in sy until sy+b.size) {
+                            val build: Building = Vars.world.build(_x, _y) ?: continue
+                            if (build is ConstructBuild || added.contains(build)) continue
+                            tileStore.setAction(DeleteAction("", build.pos(), build.block.size, this.team))
+                            added.add(build)
+                        }
+                    }
+
+                    if (b != Blocks.air) {
+                        val team: Team = this.team(instr.team) ?: Team.derelict
+                        tileStore.setAction(BuildAction("", Point2.pack(x, y), b.size, team, b, 0))
                     }
                 }
+            }
 
-                if (b != Blocks.air) {
-                    val team: Team = this.team(instr.team) ?: Team.derelict
-                    tileStore.setAction(BuildAction("", Point2.pack(x, y), b.size, team, b, 0))
+            is ControlI -> {
+                if (instr.type == LAccess.config) {
+                    val build: Building = this.obj(instr.target).takeIf { it is Building } as Building? ?: return
+                    if (!(this.privileged || (build.team == this.team && this.linkIds.contains(build.id)))) return
+                    if (!this.`var`(instr.p1).isobj) return
+                    val value: Any? = this.obj(instr.p1)
+                    tileStore.setAction(ConfigAction("", build.pos(), build.block.size, this.team, value))
                 }
             }
         }
